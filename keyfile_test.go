a61a569b509e69306d840318d1500fdfef8628c6
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-05-29 14:45:28 -0700

    Add basic unit tests for package keyfile.

---
+++ keyfile_test.go
@@ -0,0 +1,23 @@
+package keyfile_test
+
+import (
+	"testing"
+
+	"bitbucket.org/creachadair/keyfile"
+	"golang.org/x/xerrors"
+)
+
+func TestEmpty(t *testing.T) {
+	f := keyfile.New("password")
+
+	const slug = "anything"
+	if f.Has(slug) {
+		t.Errorf("Has(%q): got true, want false", slug)
+	}
+	if key, err := f.Get(slug); !xerrors.Is(err, keyfile.ErrNoSuchKey) {
+		t.Errorf("Get(%q): got %v, %v, want %v", slug, key, err, keyfile.ErrNoSuchKey)
+	}
+	if f.Remove(slug) {
+		t.Errorf("Remove(%q): got true, want false", slug)
+	}
+}
